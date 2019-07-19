c12c5aedbb0cf8bf29fdd349cec5eb44b27680e5
gueuselambix <philip@brusten.net>
2019-07-19 09:54:49 +0200

    outsources JWT-processing to https://github.com/KULeuven-CCIS/jwt-util

--- src/main/java/be/kuleuven/ccis/shibboleth/idp/authn/impl/JWTExtractUsername.java
+++ src/main/java/be/kuleuven/ccis/shibboleth/idp/authn/impl/JWTExtractUsername.java
@@ -1,11 +1,12 @@
 package be.kuleuven.ccis.shibboleth.idp.authn.impl;
 
 import be.kuleuven.ccis.shibboleth.idp.authn.context.JWTContext;
+import be.kuleuven.ccis.util.JWT;
+import be.kuleuven.ccis.util.JWTConsumer;
+import be.kuleuven.ccis.util.JWTConsumerImpl;
 import net.shibboleth.idp.authn.AbstractExtractionAction;
-import net.shibboleth.idp.authn.AbstractValidationAction;
 import net.shibboleth.idp.authn.AuthnEventIds;
 import net.shibboleth.idp.authn.context.AuthenticationContext;
-import net.shibboleth.idp.authn.principal.UsernamePrincipal;
 import net.shibboleth.utilities.java.support.annotation.constraint.NotEmpty;
 import org.opensaml.profile.action.ActionSupport;
 import org.opensaml.profile.context.ProfileRequestContext;
@@ -13,7 +14,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
-import javax.security.auth.Subject;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by philip on 28.02.2017
@@ -28,11 +30,24 @@ public class JWTExtractUsername extends AbstractExtractionAction {
 
     private JWTContext jwtCtx;
 
+    private final JWTConsumer jwtConsumer;
 
-    private final JWTProcessor jwtProcessor;
+    public JWTExtractUsername(String privatekey,
+                              List<String> jweAlgorithms,
+                              List<String> jwsAlgorithms,
+                              List<String> jweEncMethods,
+                              String jwtExpiration,
+                              Map<String,String> trustedIssuers) {
 
-    public JWTExtractUsername(JWTProcessor jwtProcessor) {
-        this.jwtProcessor = jwtProcessor;
+        this.jwtConsumer= new JWTConsumerImpl.JWTConsumerBuilder()
+                .setJWTIssuers(trustedIssuers)
+                .setExpiration(jwtExpiration)
+                .setPrivateKeyPair(privatekey)
+                .setSupportedEncryptionMethods(jweEncMethods)
+                .setSupportedJweAlgorithms(jweAlgorithms)
+                .setSupportedJwsAlgorithms(jwsAlgorithms)
+                .build();
+
     }
 
     protected boolean doPreExecute(@Nonnull ProfileRequestContext profileRequestContext, @Nonnull AuthenticationContext authenticationContext) {
@@ -58,15 +73,22 @@ public class JWTExtractUsername extends AbstractExtractionAction {
 
         log.debug("{} attempting to validate JWT {}", getLogPrefix(), jwtCtx.getJwt());
 
-        String jwtSubject = jwtProcessor.validateAndExtractSubjectFromJWT(jwtCtx.getJwt());
-
-        if (jwtSubject == null) {
-            log.warn("Could not extract user from Signed & Encrypted JWT, continuing with other authn modules");
+        JWT jwt = jwtConsumer.extract(jwtCtx.getJwt());
+        if (jwt == null) {
+            log.warn("Could not validate Signed & Encrypted JWT, continuing with other authn modules");
             ActionSupport.buildEvent(profileRequestContext, AuthnEventIds.RESELECT_FLOW);
         } else {
-            log.info("Successfuly decrypted, validated and extracted subject from JWT: {}", jwtSubject);
-            jwtCtx.setUsername(jwtSubject);
-            ActionSupport.buildProceedEvent(profileRequestContext);
+
+            String jwtSubject = jwt.getSubject();
+
+            if (jwtSubject == null) {
+                log.warn("Could not extract user from Signed & Encrypted JWT, continuing with other authn modules");
+                ActionSupport.buildEvent(profileRequestContext, AuthnEventIds.RESELECT_FLOW);
+            } else {
+                log.info("Successfuly decrypted, validated and extracted subject from JWT: {}", jwtSubject);
+                jwtCtx.setUsername(jwtSubject);
+                ActionSupport.buildProceedEvent(profileRequestContext);
+            }
         }
 
     }
